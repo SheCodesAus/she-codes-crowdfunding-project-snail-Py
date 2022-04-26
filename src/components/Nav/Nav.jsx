ee92de688753533eb78b236a94efe522ffae48c5
snail-Py <cs.legoff@gmail.com>
2022-04-26 21:04:17 +0800

    Connected Heroku API

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,7 +5,7 @@ function Nav() {
     return (
         <nav>
             <Link to="/">Home</Link>
-            <Link to="'login">Login<</Link>
+            <Link to="/login">Login</Link>
         </nav>
     )
 }
