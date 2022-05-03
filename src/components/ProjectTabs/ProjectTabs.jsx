0113f580e97fdfc667a47dc972648aa50d3e3040
snail-Py <cs.legoff@gmail.com>
2022-05-03 18:20:20 +0800

    Commented ProjectTabs out

--- src/components/ProjectTabs/ProjectTabs.jsx
+++ src/components/ProjectTabs/ProjectTabs.jsx
@@ -13,33 +13,35 @@ function ProjectTabs() {
     const { id } = useParams();
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}projects/${id}`)
-        .then((results) => {
-        return results.json();
-         })
-         .then((data) => {
-         setProjectData(data);
-         });
+            .then((results) => {
+                return results.json();
+            })
+            .then((data) => {
+                setProjectData(data);
+            });
     }, []);
 
 
     return (
-//         <Tabs>
-//     <TabList>
-//       <Tab>Title 1</Tab>
-//       <Tab>Title 2</Tab>
-//     </TabList>
+        //         <Tabs>
+        //     <TabList>
+        //       <Tab>Title 1</Tab>
+        //       <Tab>Title 2</Tab>
+        //     </TabList>
 
-//     <TabPanel>
-//       <h2>Any content 1</h2>
-//     </TabPanel>
-//     <TabPanel>
-//       <h2>Any content 2</h2>
-//     </TabPanel>
-//   </Tabs>
-// );
+        //     <TabPanel>
+        //       <h2>Any content 1</h2>
+        //     </TabPanel>
+        //     <TabPanel>
+        //       <h2>Any content 2</h2>
+        //     </TabPanel>
+        //   </Tabs>
+        // );
         
-// }
+        // }
 
         <p>Tabbed content</p>
+    );
+}
 
-export default ProjectTabs
+export default ProjectTabs;
